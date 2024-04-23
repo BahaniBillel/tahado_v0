bfcd236cf0bd0bf15b1343e25c78bd2a886a46fd
BahaniBillel <billel.bahani@gmail.com>
2024-04-23 18:23:19 +0100

    adding databases

--- client/src/app/lib/client.tsx
+++ client/src/app/lib/client.tsx
@@ -1,16 +1,30 @@
-import { HttpLink } from "@apollo/client";
-import {
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-} from "@apollo/experimental-nextjs-app-support/ssr";
-import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
+// import { HttpLink } from "@apollo/client";
+// import {
+//   NextSSRInMemoryCache,
+//   NextSSRApolloClient,
+// } from "@apollo/experimental-nextjs-app-support/ssr";
+// import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
+// export const { getClient } = registerApolloClient(() => {
+//   return new NextSSRApolloClient({
+//     cache: new NextSSRInMemoryCache(),
 
-    link: new HttpLink({
-      uri: "http://localhost:3001/graphql",
-    }),
+//     link: new HttpLink({
+//       uri: "http://localhost:3001/graphql",
+//     }),
+//   });
+// });
+
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// export const getClient = new ApolloClient({
+//   uri: "http://localhost:3001/graphql",
+//   cache: new InMemoryCache(),
+// });
+
+export function getClient() {
+  return new ApolloClient({
+    uri: "http://localhost:3001/graphql",
+    cache: new InMemoryCache(),
   });
-});
+}
--- client/src/components/headers/navigation.jsx
+++ client/src/components/headers/navigation.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React, { useState, useEffect } from "react";
 import {
   RiSearchEyeLine,
   RiHeart2Fill,
